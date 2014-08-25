c70627d6ab50840daeaf5167c6a678b7c14ce4a9
byverdu <byverdu@gmail.com>
2014-08-25 23:47:38 +0100

    test for contain_exactly method

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -50,6 +50,7 @@ describe 'testing the rspec syntax' do
 		expect(var_2).to be_a(Dog)
 	end
 
+	
 	it "playing with 'be_a_kind_of(expected)' method" do
 		
 		# Passes if actual.kind_of?(expected)
@@ -58,10 +59,13 @@ describe 'testing the rspec syntax' do
 
 		expect('some String').to be_a_kind_of(var_0)
 		expect(var_1).to         be_a_kind_of(String)
-		expect(var_0).not_to     be_a_kind_of(Fixnum)    
+		expect(var_1).not_to     be_a_kind_of(Fixnum)   
+
+		# Is also possible use 'be_an_instance_of(expected)' 
 	end
 
-	it "playing with 'be_between(min,max)'" do
+	
+	it "playing with 'be_between(min,max)' method" do
 		
 		# Works with any Comparable object --> String, Symbol, Time or Numeric
 		# 'Inclusive' min and max by default, if desired append 'exclusive' at the end of the chain
@@ -69,6 +73,41 @@ describe 'testing the rspec syntax' do
 		expect(2.3).to   be_between(1, 55) 
 		expect(11 ).to   be_between(10,12).exclusive
 		expect(9).not_to be_between(10,15)
+	end
+
+	
+	it "playing with 'be_within' method" do
+		
+		var = 8*8
+
+		expect(6).to   be_within(3.4).of(8)
+		expect(var).to be_within(60).of(66)
+	end
+
+
+	xit "playing with 'change(receiver = nil, message = nil, &block)' method" do
+		
+		# Applied to a proc, specifies that its execution will cause some value to change.
+		# You can either pass receiver and message, or a block(use '{}'), but not both. 
+
+	end
+
+
+	it "playing with 'contain_exactly(*items)' method" do
+		
+		# Passes if actual collections contains all of the expected regardless of order. 
+
+		var_0, var_1 = ['Albert',34], ['Superman']
+
+		expect(var_0).to  contain_exactly(String,Fixnum)
+		expect(var_1).to  contain_exactly('Superman')
+	end
+
+
+	it "playing with 'cover(*values)' method" do
+		
+		# Passes if actual covers expected. This works for Ranges
+
 
 	end
 end
